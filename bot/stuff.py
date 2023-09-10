8983a7537a556894a36b2b2b4e396bbe6c50883a
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-09-10 10:37:01 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -53,19 +53,11 @@ async def help(event):
     await event.edit(
         f"""**Send Me Any Video I Will Automatically Send It's Compressed Video File 😎\n\nSince I am made by @Prime_Hritu • @Private_Bots**""",
         buttons=[
-            [Button.inline("Back 🔙", data="starti")],
+            [Button.url("Updates Channel 🇮🇳", url="t.me/Private_Bots")],
         ],
     )
     
 async def ihelp(e):
     await e.reply(
         f"""**Send Me Any Video I Will Automatically Send It's Compressed Video File 😎\n\nSince I am made by @Prime_Hritu • @Private_Bots**"""
-    )
-async def starti(event):
-    await event.edit(
-        f"**Send me the video which you want to compress.**\n**Uptime: {str(datetime.now() - START_TIME).split('.')[0]}\n\nA Bot From @Private_Bots\nMade By @Prime_Hritu**",
-        buttons=[
-            [Button.inline("HELP", data="help")],
-            [Button.url("Updates Channel 🇮🇳",url="t.me/Private_Bots")],
-        ],
     )
