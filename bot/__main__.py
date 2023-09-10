210b4ca9264d05a219843d8385d0c59a7dd88008
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-09-10 08:02:46 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -131,8 +131,6 @@ async def _(e):
 
 @bot.on(events.NewMessage(incoming=True))
 async def _(event):
-        if str(event.sender_id) not in OWNER and event.sender_id !=DEV:
-            return await event.reply_text("**Sorry You're not An Authorised User!**")
         if not event.photo:
             return
         os.system("rm thumb.jpg")
